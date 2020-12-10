a3d7bff17d8b6ed8fbeb17231e1c2e5a50d9bed1
Aloner <aloner112@gmail.com>
2020-12-10 17:18:38 +0800

    feat: FolderPath檢查(假

---
+++ Assets/Plugins/OOOneUnityTools/Tests/Editor/FolderPathTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace OOOneTools.Editor.Tests
+{
+    public class FolderPathTest
+    {
+        [Test]
+        public void FolderPathExist()
+        {
+            FolderPathHandler folderPathHandler = new FolderPathHandler();
+            Assert.AreEqual(true, folderPathHandler.IsFolderExist());
+        }
+
+
+    }
+}
