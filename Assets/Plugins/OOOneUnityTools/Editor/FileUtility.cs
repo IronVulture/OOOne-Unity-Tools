5ec583cd322c84e2909927c9092a2ca14ffc5a1c
Unknown <cycleapple@gmail.com>
2020-12-11 03:17:10 +0800

    feat: CreateTestPngByPath()

---
+++ Assets/Plugins/OOOneUnityTools/Editor/FileUtility.cs
@@ -0,0 +1,22 @@
+﻿
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace OOOneUnityTools.Editor
+{
+    public class FileUtility
+    {
+        public static void CreatTestPngByPath(string path, string fileName)
+        {
+            string fullPath = path + @"\" + fileName + ".png";
+
+            Texture2D testTexture = new Texture2D(10, 10);
+            testTexture.Apply();
+            var Bytes = testTexture.EncodeToPNG();
+            File.WriteAllBytes(fullPath, Bytes);
+            AssetDatabase.Refresh();
+        }
+
+    }
+}
