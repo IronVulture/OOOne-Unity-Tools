1d6a21a55fc05bf7c579e0e8d279f76b81a352d2
Aloner <aloner112@gmail.com>
2020-12-10 18:32:00 +0800

    feat : implement FolderExist check

--- Editor/FileUtility.cs
+++ Editor/FileUtility.cs
@@ -1,13 +1,17 @@
+using OOOneTools.Editor;
 using UnityEngine;
 
 namespace OOOneUnityTools.Editor
 {
     public class FileUtility
     {
-        public bool TryGetAllOverrideControllerByFolder(string folderPath , out AnimatorOverrideController[] animatorOverrideControllers)
+        public bool TryGetAllOverrideControllerByFolder(string                           folderPath ,
+                                                        out AnimatorOverrideController[] animatorOverrideControllers)
         {
+            var folderPathHandler = new FolderPathHandler();
+            var isFolderExist     = folderPathHandler.IsFolderExist(folderPath);
             animatorOverrideControllers = new AnimatorOverrideController[3];
-            return true;
+            return isFolderExist;
         }
     }
 }
