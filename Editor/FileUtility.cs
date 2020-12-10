a09f2d730351c78e450ca2e42c426f8858d5f59e
rStar <starlove124760@gmail.com>
2020-12-10 18:41:49 +0800

    feat : add FileUtility and Tests

--- Editor/FileUtility.cs
+++ Editor/FileUtility.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
+<<<<<<< refs/remotes/origin/feature/在資料夾內找單個或多個指定類型的檔案
 namespace OOOneUnityTools.Editor
+=======
+namespace Plugins.OOOneUnityTools.Editor
+>>>>>>> feat : add FileUtility and Tests
 {
     public class FileUtility
     {
