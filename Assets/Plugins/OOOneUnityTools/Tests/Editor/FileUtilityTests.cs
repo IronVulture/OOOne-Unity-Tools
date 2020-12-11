1698d381f136671fcf7922c92c184820579c4be3
CY <cycleapple@gmail.com>
2020-12-11 14:40:22 +0800

    merge: 將feature線合併

--- Assets/Plugins/OOOneUnityTools/Tests/Editor/FileUtilityTests.cs
+++ Assets/Plugins/OOOneUnityTools/Tests/Editor/FileUtilityTests.cs
@@ -1,7 +1,9 @@
 #region
 
+using System.IO;
 using NUnit.Framework;
 using OOOne.Tools.Editor;
+using UnityEngine;
 
 #endregion
 
@@ -10,12 +12,22 @@ namespace OOOneTools.Editor.Tests
     public class FileUtilityTests
     {
         private FileUtility _fileUtility;
+        private string _fileName;
+        private string _absoluteFolderPath;
+        private string _pngFileExtension;
+        private string _unityAssetsFolderPath;
+
 
         [SetUp]
         public void SetUp()
         {
             _fileUtility = new FileUtility();
+            _fileName = "apple";
+            _pngFileExtension = ".png";
+            _absoluteFolderPath = Application.dataPath;
+            _unityAssetsFolderPath = "Assets/";
         }
+        
 
         [Test]
         public void Get_All_OverrideController_By_Folder()
@@ -25,6 +37,45 @@ namespace OOOneTools.Editor.Tests
                                                                            , out var overrideControllers);
             Assert.AreEqual(true , hasFile);
             Assert.NotNull(overrideControllers);
+        }
+
+        [Test]
+        public void ParsePathUnityToCsharpTests()
+        {
+            const string pathBeforeParse = "Assets/Plugins/OOOneUnityTools/Editor/FileUtility.cs";
+            const string expectedResult = @"Assets\Plugins\OOOneUnityTools\Editor\FileUtility.cs";
+            var pathAfterParse = FileUtility.ParsePathUnityToCsharp(pathBeforeParse);
+            Assert.AreEqual(expectedResult, pathAfterParse);
+        }
+
+
+        [Test]
+        public void DeleteTestsPngFromPathTests()
+        {
+            var metaPathBeforeParse = _absoluteFolderPath + "/" + _fileName + ".meta";
+            var metaPathAfterParse = FileUtility.ParsePathUnityToCsharp(metaPathBeforeParse);
+            var pngPathBeforeParse = _absoluteFolderPath + "/" + _fileName + _pngFileExtension;
+            var pngPathAfterParse = FileUtility.ParsePathUnityToCsharp(pngPathBeforeParse);
+            FileUtility.CreatTestPngByPath(_absoluteFolderPath, _fileName);
+            FileUtility.DeleteFileWithMetaByPath(_absoluteFolderPath, _fileName, ".png");
+            var metaFileExist = File.Exists(metaPathAfterParse);
+            var pngFileExist = File.Exists(pngPathAfterParse);
+            Assert.IsFalse(metaFileExist);
+            Assert.IsFalse(pngFileExist);
+        }
+
+
+        [Test]
+        public void CreateTexturePresetToPathTests()
+        {
+            var presetPath = _unityAssetsFolderPath;
+            const string presetFileName = "testPreset";
+            const string presetFileExtension = ".preset";
+            FileUtility.CreateTestTexturePreset(presetPath, presetFileName);
+            var absoluteFullPath = _absoluteFolderPath + presetFileName + presetFileExtension;
+            var presetFileExist = File.Exists(absoluteFullPath);
+            Assert.IsFalse(presetFileExist);
+            FileUtility.DeleteFileWithMetaByPath(_absoluteFolderPath, presetFileName, presetFileExtension);
         }
     }
 }
