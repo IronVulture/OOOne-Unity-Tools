065628df180921431c96d21d27ea53a7bc08b1d9
CY <cycleapple@gmail.com>
2020-12-11 11:58:57 +0800

    feat: DeleteFileWithMetaByPath now require fileExtension

--- Assets/Plugins/OOOneUnityTools/Tests/Editor/SetTextureSetting.cs
+++ Assets/Plugins/OOOneUnityTools/Tests/Editor/SetTextureSetting.cs
@@ -27,16 +27,16 @@ namespace Tests
             folderPath = Application.dataPath;
             assetPath = "Assets/" + fileName + ".png";
             pngPathBeforeParse = folderPath + "/" + fileName + ".png";
-            pngPathAfterParse = pngPathBeforeParse.Replace("/", @"\");
+            pngPathAfterParse = FileUtility.ParsePathUnityToCsharp(pngPathBeforeParse);
             metaPathBeforeParse = folderPath + "/" + fileName + ".meta";
-            metaPathAfterParse = metaPathBeforeParse.Replace("/", @"\");
+            metaPathAfterParse = FileUtility.ParsePathUnityToCsharp(metaPathBeforeParse);
         }
 
         [Test]
         public void ParsePathUnityToCsharpTests()
         {
-            var pathBeforeParse = "Assets/Plugins/OOOneUnityTools/Editor/FileUtility.cs";
-            var expectedResult = @"Assets\Plugins\OOOneUnityTools\Editor\FileUtility.cs";
+            const string pathBeforeParse = "Assets/Plugins/OOOneUnityTools/Editor/FileUtility.cs";
+            const string expectedResult = @"Assets\Plugins\OOOneUnityTools\Editor\FileUtility.cs";
             var pathAfterParse = FileUtility.ParsePathUnityToCsharp(pathBeforeParse);
             Assert.AreEqual(expectedResult, pathAfterParse);
         }
@@ -46,7 +46,7 @@ namespace Tests
         public void DeleteTestsPngFromPathTests()
         {
             FileUtility.CreatTestPngByPath(folderPath, fileName);
-            FileUtility.DeleteFileWithMetaByPath(folderPath, fileName);
+            FileUtility.DeleteFileWithMetaByPath(folderPath, fileName, ".png");
             var texture2D = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
             var metaFileExist = File.Exists(metaPathAfterParse);
             var pngFileExist = File.Exists(pngPathAfterParse);
@@ -62,7 +62,7 @@ namespace Tests
             FileUtility.CreatTestPngByPath(folderPath, fileName);
             var texture2D = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
             Assert.IsNotNull(texture2D);
-            FileUtility.DeleteFileWithMetaByPath(folderPath, fileName);
+            FileUtility.DeleteFileWithMetaByPath(folderPath, fileName, ".png");
         }
 
 
