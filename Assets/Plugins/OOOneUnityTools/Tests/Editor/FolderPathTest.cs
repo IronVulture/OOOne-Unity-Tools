5f18875fd8a06ba57f7970d7f1b9ca8af0f8451c
Aloner <aloner112@gmail.com>
2020-12-11 10:58:55 +0800

    refactor: folderPathTest extract method, add comment

--- Assets/Plugins/OOOneUnityTools/Tests/Editor/FolderPathTest.cs
+++ Assets/Plugins/OOOneUnityTools/Tests/Editor/FolderPathTest.cs
@@ -27,37 +27,62 @@ namespace OOOneTools.Editor.Tests
             _targetFolder2Delete = baseFolderPath + "/Folder2Delete1/Folder2Delete2";
         }
 
+        //測試檢查資料夾是否存在
         [Test]
         public void FolderPathExist()
         {
             //預期不存在的folderPath
-            Assert.AreEqual(false, _folderPathHandler.IsFolderExist(_folderPathNotExist));
+            FolderShouldNotExist(_folderPathNotExist);
             //預期存在的folderPath
-            Assert.AreEqual(true, _folderPathHandler.IsFolderExist(_ExistingFolderPath));
+            FolderShouldExist(_ExistingFolderPath);
         }
 
+        //測試創建資料夾
         [Test]
         public void CreateFolderIfNotExist()
         {
-            Assert.AreEqual(false, _folderPathHandler.IsFolderExist(_targetFolderPath));
+            //創建資料夾前資料夾不應該存在
+            FolderShouldNotExist(_targetFolderPath);
             _folderPathHandler.CreateFolderIfNotExist(_targetFolderPath);
-            Assert.AreEqual(true, _folderPathHandler.IsFolderExist(_targetFolderPath));
+            //創建資料夾後，資料夾應該存在
+            FolderShouldExist(_targetFolderPath);
         }
 
+        //測試刪除資料夾
         [Test]
         public void DeleteTargetFolder()
         {
+            //刪除前先創建要刪除的資料夾（兩層）
             _folderPathHandler.CreateFolderIfNotExist(_targetFolder2Delete);
-            Assert.AreEqual(true, _folderPathHandler.IsFolderExist(_targetFolder2Delete));
+            //刪除前，要刪除的內層資料夾應該要存在
+            FolderShouldExist(_targetFolder2Delete);
+
             _folderPathHandler.DeleteTargetFolder(_targetFolder2Delete);
-            Assert.AreEqual(false, _folderPathHandler.IsFolderExist(_targetFolder2Delete));
-            Assert.AreEqual(true, _folderPathHandler.IsFolderExist(_targetFolder2DeleteBase));
+
+            //刪除後，要刪除的內層資料夾不應該存在
+            FolderShouldNotExist(_targetFolder2Delete);
+            //刪除後，沒有要刪除的外層資料夾應該存在
+            FolderShouldExist(_targetFolder2DeleteBase);
+        }
+
+        //檢查資料夾是否如預期的存在
+        private void FolderShouldExist(string folderPath)
+        {
+            Assert.AreEqual(true, _folderPathHandler.IsFolderExist(folderPath));
+        }
+
+        //檢查資料夾是否如預期的不存在
+        private void FolderShouldNotExist(string folderPath)
+        {
+            Assert.AreEqual(false, _folderPathHandler.IsFolderExist(folderPath));
         }
 
         [TearDown]
         public void TearDown()
         {
+            //刪除「測試刪除資料夾」產生的資料夾
             _folderPathHandler.DeleteTargetFolder(_targetFolder2DeleteBase);
+            //刪除「測試創建資料夾」產生的資料夾
             _folderPathHandler.DeleteTargetFolder(_targetFolderPath = baseFolderPath + "/Test2");
         }
 
