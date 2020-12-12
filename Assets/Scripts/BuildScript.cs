b73abe1ae54f55a0b2f573df361793e042e4b7c2
builder001studio <builder@001studio.com.tw>
2020-12-13 03:11:10 +0800

    feat: add buildscript with amsdef

---
+++ Assets/Scripts/BuildScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+public class BuildScript 
+{
+    [MenuItem("MyTools/Switch Build")]
+    public static void BuildGame ()
+    {
+        // Get filename.
+        string[] scenes = new string[] {"Assets/Scenes/SampleScence.unity"};
+
+        // Build player.
+        BuildPipeline.BuildPlayer(scenes, "./builds/OOOneTools", BuildTarget.Switch, BuildOptions.Development);
+        
+        
+    }
+}
