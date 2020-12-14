e8c6a3510cd813fd115a8a5d201b6a6a6eb2e4c9
rStar <starlove124760@gmail.com>
2020-12-15 04:25:04 +0800

    remove commmant

--- Assets/ExampleBehavior/Attribute/StringInListDrawer.cs
+++ Assets/ExampleBehavior/Attribute/StringInListDrawer.cs
@@ -9,26 +9,12 @@ public class StringInList : PropertyAttribute
 {
     public delegate string[] GetStringList();
 
-
-    public StringInList(params string[] list)
-    {
-        List = list;
-    }
+    public StringInList(params string[] list) => List = list;
 
     public StringInList(Type type , string methodName)
     {
         _type       = type;
         _methodName = methodName;
-        // var method = type.GetMethod(methodName);
-        //
-        // if (method != null)
-        // {
-        //     // List = method.Invoke(null , null) as string[];
-        // }
-        // else
-        // {
-        //     Debug.LogError("NO SUCH METHOD " + methodName + " FOR " + type);
-        // }
     }
 
     private Type     _type;
--- Assets/ExampleBehavior/ExampleBehavior.cs
+++ Assets/ExampleBehavior/ExampleBehavior.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
-public class ExampleBehavior : MonoBehaviour {
+public class ExampleBehavior : MonoBehaviour
+{
     // This will store the string value
     // [StringInList("Cat", "Dog")] public string Animal;
     // This will store the index of the array value
     // [StringInList("John", "Jack", "Jim")] public int PersonID;
-    
+
     // Showing a list of loaded scenes
-    [StringInList(typeof(PropertyDrawersHelper), "AllSceneNames")] public string SceneName;
+    [StringInList(typeof(PropertyDrawersHelper) , "AllSceneNames")]
+    public int SceneName;
 }
