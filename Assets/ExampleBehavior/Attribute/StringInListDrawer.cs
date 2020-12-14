3f9ccfd9699280092cb6be391bbf5140cb7eeafc
rStar <starlove124760@gmail.com>
2020-12-15 03:54:13 +0800

    fixed : nameList not update in value change

--- Assets/ExampleBehavior/Attribute/StringInListDrawer.cs
+++ Assets/ExampleBehavior/Attribute/StringInListDrawer.cs
@@ -2,48 +2,88 @@ using System;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
+
 #endif
 
-public class StringInList : PropertyAttribute {
-  public delegate string[] GetStringList();
+public class StringInList : PropertyAttribute
+{
+    public delegate string[] GetStringList();
 
-  public StringInList(params string [] list) {
-    List = list;
-  }
 
-  public StringInList(Type type, string methodName) {
-    var method = type.GetMethod (methodName);
-    if (method != null) {
-      List = method.Invoke (null, null) as string[];
-    } else {
-      Debug.LogError ("NO SUCH METHOD " + methodName + " FOR " + type);
+    public StringInList(params string[] list)
+    {
+        List = list;
     }
-  }
 
-  public string[] List {
-    get;
-    private set;
-  }
+    public StringInList(Type type , string methodName)
+    {
+        _type       = type;
+        _methodName = methodName;
+        // var method = type.GetMethod(methodName);
+        //
+        // if (method != null)
+        // {
+        //     // List = method.Invoke(null , null) as string[];
+        // }
+        // else
+        // {
+        //     Debug.LogError("NO SUCH METHOD " + methodName + " FOR " + type);
+        // }
+    }
+
+    private Type     _type;
+    private string   _methodName;
+    private string[] _list;
+
+    public string[] List
+    {
+        get
+        {
+            if (_list != null)
+                return _list;
+
+            var method = _type.GetMethod(_methodName);
+
+            if (method != null)
+            {
+                // List = method.Invoke(null , null) as string[];
+            }
+            else
+            {
+                Debug.LogError("NO SUCH METHOD " + _methodName + " FOR " + _type);
+            }
+
+            return method.Invoke(null , null) as string[];
+        }
+        set => _list = value;
+    }
 }
 
 #if UNITY_EDITOR
 [CustomPropertyDrawer(typeof(StringInList))]
-public class StringInListDrawer : PropertyDrawer {
-  // Draw the property inside the given rect
-  public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
-    var stringInList = attribute as StringInList;
-    Debug.Log($"{stringInList.List.Length}");
-    var list = stringInList.List;
-    if (property.propertyType == SerializedPropertyType.String) {
-      int index = Mathf.Max (0, Array.IndexOf (list, property.stringValue));
-        index = EditorGUI.Popup (position, property.displayName, index, list);
+public class StringInListDrawer : PropertyDrawer
+{
+    // Draw the property inside the given rect
+    public override void OnGUI(Rect position , SerializedProperty property , GUIContent label)
+    {
+        var stringInList = attribute as StringInList;
+        Debug.Log($"{stringInList.List.Length}");
+        var list = stringInList.List;
+        if (property.propertyType == SerializedPropertyType.String)
+        {
+            int index = Mathf.Max(0 , Array.IndexOf(list , property.stringValue));
+            index = EditorGUI.Popup(position , property.displayName , index , list);
 
-      property.stringValue = list [index];
-    } else if (property.propertyType == SerializedPropertyType.Integer) {
-      property.intValue = EditorGUI.Popup (position, property.displayName, property.intValue, list);
-    } else {
-      base.OnGUI (position, property, label);
+            property.stringValue = list[index];
+        }
+        else if (property.propertyType == SerializedPropertyType.Integer)
+        {
+            property.intValue = EditorGUI.Popup(position , property.displayName , property.intValue , list);
+        }
+        else
+        {
+            base.OnGUI(position , property , label);
+        }
     }
-  }
 }
 #endif
