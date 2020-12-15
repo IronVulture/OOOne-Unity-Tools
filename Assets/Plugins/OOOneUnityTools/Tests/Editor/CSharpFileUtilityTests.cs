f6894edc2b4889c8176215915fc497c972cda671
CY <cycleapple@gmail.com>
2020-12-15 18:28:30 +0800

    fix: old test withoud create and delete files

--- Assets/Plugins/OOOneUnityTools/Tests/Editor/CSharpFileUtilityTests.cs
+++ Assets/Plugins/OOOneUnityTools/Tests/Editor/CSharpFileUtilityTests.cs
@@ -1,30 +1,38 @@
+using System.IO;
 using NUnit.Framework;
+using Plugins.OOOneUnityTools.Editor;
 
 namespace OOOneTools.Editor.Tests
 {
     public class CSharpFileUtilityTests
     {
         private string _beforeParsePath;
+        private string _childPath;
+        private string _fileName;
 
 
         [SetUp]
         public void SetUp()
         {
-            _beforeParsePath = "Assets/New Folder";
+            _beforeParsePath = "Assets/asdfasdlfja";
+            _childPath = "asdfasdlfja";
+            _fileName = "235432asdfasdf";
         }
 
         [Test]
         public void IsFolderExist()
         {
 
+            Directory.CreateDirectory(UnityPathUtility.GetCsharpUnityAbsoluteFolderPath(_childPath));
             var isFolderExist = CSharpFileUtility.IsFolderExist(_beforeParsePath);
             Assert.IsTrue(isFolderExist);
+            UnityFileUtility.DeleteUnityFolder(_childPath);
         }
 
         [Test]
         public void ParseSlashToCsharpTest()
         {
-            var afterParse = @"Assets\New Folder";
+            var afterParse = @"Assets\asdfasdlfja";
             var actual = CSharpFileUtility.ParseSlashToCsharp(_beforeParsePath);
             Assert.AreEqual(afterParse, actual);
         }
@@ -32,9 +40,20 @@ namespace OOOneTools.Editor.Tests
         [Test]
         public void IsFileInPath()
         {
-            var isFileInPath = CSharpFileUtility.IsFileInPath(_beforeParsePath, "TestFile", "txt");
+            Directory.CreateDirectory(UnityPathUtility.GetCsharpUnityAbsoluteFolderPath(_childPath));
+            UnityFileUtility.CreateAssetFile(UnityFileUtility.FileType.Png, _childPath, _fileName);
+            const string extension = "png";
+            var isFileInPath = CSharpFileUtility.IsFileInPath(_beforeParsePath, _fileName, extension);
             Assert.AreEqual(true, isFileInPath);
         }
+
+        // [Test]
+        // public void CopyFile_If_Folder_Exist_And_File_NotExist()
+        // {
+        //     string pathA = "";
+        //     string pathB = "";
+        //     Assert.AreEqual( true , CSharpFileUtility.IsFileAreEqual(pathA, pathB) );
+        // }
 
     }
 }
