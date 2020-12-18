c5b00e8da5152f2c7c9633334b829e0399fc43ef
CY <cycleapple@gmail.com>
2020-12-18 16:21:24 +0800

    refactor : extract method - ShouldEqualResult to UnityPathUtilityTests

--- Assets/Plugins/OOOneUnityTools/Tests/Editor/UnityPathUtilityTests.cs
+++ Assets/Plugins/OOOneUnityTools/Tests/Editor/UnityPathUtilityTests.cs
@@ -5,8 +5,14 @@ namespace OOOneUnityTools.Editor.Tests
 {
     public class UnityPathUtilityTests
     {
+        #region Private Variables
+
         private static string _childPath;
         private static string _fileName;
+
+        #endregion
+
+        #region Setup/Teardown Methods
 
         [SetUp]
         public void SetUp()
@@ -15,38 +21,42 @@ namespace OOOneUnityTools.Editor.Tests
             _fileName = "asdfeervsdklfjol";
         }
 
+        #endregion
+
+        #region Test Methods
+
         [Test]
         public static void GetUnityAbsoluteFullPath()
         {
             var extension = "overrideController";
             var expected = $"{Application.dataPath}/{_childPath}/{_fileName}.{extension}";
-            UnityFileUtilityTests.ShouldEqualResult(expected,
+            ShouldEqualResult(expected,
                 UnityPathUtility.GetUnityAbsoluteFullPath(_childPath, _fileName, extension));
         }
 
         [Test]
         public static void GetUnityPath()
         {
-            UnityFileUtilityTests.ShouldEqualResult("Assets", UnityPathUtility.GetUnityPath());
+            ShouldEqualResult("Assets", UnityPathUtility.GetUnityPath());
         }
 
         [Test]
         public static void GetUnityAbsolutePath()
         {
-            UnityFileUtilityTests.ShouldEqualResult(Application.dataPath, UnityPathUtility.GetAbsolutePath());
+            ShouldEqualResult(Application.dataPath, UnityPathUtility.GetAbsolutePath());
         }
 
         [Test]
         public static void GetUnityFolderPath()
         {
-            UnityFileUtilityTests.ShouldEqualResult("Assets/" + _childPath,
+            ShouldEqualResult("Assets/" + _childPath,
                 UnityPathUtility.GetUnityFolderPath(_childPath));
         }
 
         [Test]
         public static void GetUnityAbsoluteFolderPath()
         {
-            UnityFileUtilityTests.ShouldEqualResult($"{Application.dataPath}/{_childPath}",
+            ShouldEqualResult($"{Application.dataPath}/{_childPath}",
                 UnityPathUtility.GetUnityAbsoluteFolderPath(_childPath));
         }
 
@@ -55,8 +65,19 @@ namespace OOOneUnityTools.Editor.Tests
         {
             var extension = "overrideController";
             var expected = $"Assets/{_childPath}/{_fileName}.{extension}";
-            UnityFileUtilityTests.ShouldEqualResult(expected,
+            ShouldEqualResult(expected,
                 UnityPathUtility.GetUnityFullPath(_childPath, _fileName, extension));
         }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void ShouldEqualResult(string expected, string result)
+        {
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
     }
 }
